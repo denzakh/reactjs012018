a8995dcd27e9102ba1f67a55622a249666615607
denzakh <den.zakh@gmail.com>
2018-02-04 00:07:34 +0300

    dz3 start

--- src/App.js
+++ src/App.js
@@ -1,91 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import PersonalForm from './PersonalForm';
-import CardForm from './CardForm';
-import Step from './Step';
+import Switcher from './Switcher';
+import CardNumberHolder from './CardNumberHolder';
+import ModalButton from './ModalButton';
+import VideoPlayer from './VideoPlayer';
 
-
-
-export default 
-class App extends React.Component {
-  state = { step: 1, firstName: "", lastName: "", email: "", cardNumber: "" };
-
-  handleTabClick = (step) => {
-    this.setState({ step: step });
-  };
-
-  handleChangeForm = (label, value) => {
-    this.setState({ [label]: value });
-  };
-
-  handleClickNextForm = e => {
-    this.setState({
-      step: this.state.step + 1
-    });
-  };
-
-  isFormCommitable = () => {
-    if (this.state.step === 1) {
-      if (this.state.firstName !== "" && this.state.lastName !== "" && this.state.email !== "" && this.state.email.includes("@")) {
-        return true;
-      } else {
-        return false;
-      }
-    } else if (this.state.step === 2) {
-      if (this.state.cardNumber.length === 16) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  }
-
-  renderForm = () => {
-    if(this.state.step === 1) {
-      return <PersonalForm onChangeForm={this.handleChangeForm} firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} />;
-    } else if (this.state.step === 2) {
-      return <CardForm onChangeForm={this.handleChangeForm} cardNumber={this.state.cardNumber} />;
-    } else {
-      return "Поздравляем!";
-    }
-  }
-
+class App extends Component {
   render() {
-    let stepsArr = [
-      { number: 1, title: "Personal information" }, 
-      { number: 2, title: "Card information" }, 
-      { number: 3, title: "Finish" }
-    ];
-
-    let steps = stepsArr.map( (step) => {
-      let isSelected;
-      if (step.number === this.state.step) {
-        isSelected = true;
-      } else {
-        isSelected = false;
-      }
-      return <Step number={step.number} key={step.title} children={step.title} onClick={this.handleTabClick} isClickable={this.isFormCommitable()} isSelected={isSelected} />;      
-    });
-
-    return <div>
-        <div className="container">
-          <div className="tab-panel">{steps}</div>
-          <div className="form-content">{this.renderForm()}</div>
-          <div className="button-panel">
-            <button className="button-next" onClick={this.handleClickNextForm} disabled={!this.isFormCommitable()}>
-              Next
-            </button>
-          </div>
-        </div>
-      </div>;
+    return (
+      <Switcher>
+        <VideoPlayer />
+        <CardNumberHolder />
+        <ModalButton />
+      </Switcher>
+    );
   }
 }
 
-
-
-
-
-
-
+export default App;
