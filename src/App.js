02645a468105c0baa329ca2e59a1fd1659d21dd6
denzakh <den.zakh@gmail.com>
2018-02-03 18:15:24 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import PersonalForm from './PersonalForm';
+import PersonalForm from './PersonalForm';
 // import CardForm from './CardForm';
 import Step from './Step';
 
@@ -57,22 +57,22 @@ class App extends React.Component {
       } else {
         isSelected = false;
       }
-      return <Step number={step.number} children={step.title} onClick={this.handleTabClick} isClickable={this.isFormCommitable()} isSelected={isSelected} />      
+      return <Step number={step.number} key={step.title} children={step.title} onClick={this.handleTabClick} isClickable={this.isFormCommitable()} isSelected={isSelected} />;      
     });
 
-    return (<div>
+    return <div>
         <div className="container">
-          <div className="tab-panel">
-            {steps}
+          <div className="tab-panel">{steps}</div>
+          <div className="form-content">
+            <PersonalForm onChangeForm={this.handleChangeForm} firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} />
           </div>
-          <div className="form-content">.</div>
           <div className="button-panel">
-            <button className="button-next" onClick={this.handleClickNextForm}>
+            <button className="button-next" onClick={this.handleClickNextForm} disabled={!this.isFormCommitable()}>
               Next
             </button>
           </div>
         </div>
-      </div>)
+      </div>;
   }
 }
 
--- src/PersonalForm.js
+++ src/PersonalForm.js
@@ -1,4 +1,23 @@
 import React from "react";
 import "./PersonalForm.css";
 
-export default class PersonalForm extends React.Component {}
+export default class PersonalForm extends React.Component {
+
+    handleChangeForm = (e) => {        
+        let fnChangeForm = this.props.onChangeForm;
+        let name = e.target.name;
+        let value = e.target.value;
+        fnChangeForm(name, value);
+    }
+
+    render () {
+
+        return <div className="personal-form">
+            <h1 className="title">Персональная информация</h1>
+            <input placeholder="First name" name="firstName" value={this.props.firstName} onChange={this.handleChangeForm} />
+            <input placeholder="Last name" name="lastName" value={this.props.lastName} onChange={this.handleChangeForm} />
+            <input placeholder="Email" name="email" value={this.props.email} onChange={this.handleChangeForm} />
+          </div>;
+
+    }
+}
